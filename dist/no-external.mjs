89a1f3836d200410ee64f178e2520dd4e71bd207
mariusmariusmariusmariusmarius <marius@qc-realestate.de>
2026-02-14 19:01:18 +0100

    feat: initial puck-core v0.21.1 dist

---
+++ dist/no-external.mjs
@@ -0,0 +1,83 @@
+import {
+  AutoField,
+  Button,
+  Drawer,
+  DropZone,
+  FieldLabel,
+  Puck,
+  Render,
+  blocksPlugin,
+  createUsePuck,
+  fieldsPlugin,
+  legacySideBarPlugin,
+  outlinePlugin,
+  overrideKeys,
+  registerOverlayPortal,
+  renderContext,
+  setDeep,
+  useGetPuck,
+  usePuck
+} from "./chunk-XK5MTSKZ.mjs";
+import "./chunk-37ENU3PE.mjs";
+import {
+  migrate,
+  resolveAllData,
+  transformProps
+} from "./chunk-6RM6VUMX.mjs";
+import "./chunk-SQT3TR4R.mjs";
+import {
+  Action,
+  ActionBar,
+  Group,
+  IconButton,
+  Label,
+  RichTextMenu,
+  Separator
+} from "./chunk-EDAKE6U3.mjs";
+import "./chunk-WXKLACBB.mjs";
+import "./chunk-R6DVFYN7.mjs";
+import "./chunk-CV6RC2N3.mjs";
+import "./chunk-4YHCXWZF.mjs";
+import "./chunk-37HTE4KO.mjs";
+import "./chunk-MDUBGHWF.mjs";
+import "./chunk-Y2EFNT5P.mjs";
+import {
+  walkTree
+} from "./chunk-PMXRXC2B.mjs";
+import {
+  init_react_import
+} from "./chunk-M6W7YEVX.mjs";
+
+// bundle/no-external.ts
+init_react_import();
+export {
+  Action,
+  ActionBar,
+  AutoField,
+  Button,
+  Drawer,
+  DropZone,
+  FieldLabel,
+  Group,
+  IconButton,
+  Label,
+  Puck,
+  Render,
+  RichTextMenu,
+  Separator,
+  blocksPlugin,
+  createUsePuck,
+  fieldsPlugin,
+  legacySideBarPlugin,
+  migrate,
+  outlinePlugin,
+  overrideKeys,
+  registerOverlayPortal,
+  renderContext,
+  resolveAllData,
+  setDeep,
+  transformProps,
+  useGetPuck,
+  usePuck,
+  walkTree
+};
